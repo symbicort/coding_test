71ef145feab4a31ac0283fb2f257ba6b4b0fb972
symbicort <jeongwonlee0@gmail.com>
2024-10-27 23:38:11 +0900

    코딩 문제 추가

--- solve_problem/1157.js
+++ solve_problem/1157.js
@@ -1,5 +1,23 @@
 const data = require('fs').readFileSync('example.txt').toString().trim().toUpperCase().split('')
 
-let maxvalue = 0
+let maxLength = 0;
+let maxValue = '';
 
-console.log(data.indexOf(data[4]))
+const filterData = data.filter((element, index) => {
+    return data.indexOf(element) === index
+})
+
+for(let i = 0; i < filterData.length; i++){
+    const roopLength = data.filter(element => filterData[i] === element).length;
+    if(roopLength === maxLength){
+        maxValue = '?'
+    } else if(roopLength > maxLength){
+        maxValue = filterData[i]
+        maxLength = roopLength
+    } else {
+        continue;
+    }
+}
+
+console.log(maxValue)
+
