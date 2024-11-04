bc5198c3702dd3125e6e1531e3a3e32ed68aa93c
symbicort <jeongwonlee0@gmail.com>
2024-11-04 22:55:40 +0900

    문제 풀이 주석 추가

--- solve_problem/2563.js
+++ solve_problem/2563.js
@@ -7,7 +7,6 @@ for(let i = 1; i < Number(data[0]); i++){
     for(let j = i + 1; j <= Number(data[0]); j++ ){
         const arr1 = data[i].split(' ').map(Number)
         const arr2 = data[j].split(' ').map(Number)
-        // 겹치는 부분 계산 로직
         if(arr1[0] - arr2[0] < 10 && arr1[0] - arr2[0] > -10){
             dupArr.push([(10 + Math.min(arr1[0], arr2[0])) - Math.max(arr1[0], arr2[0]), 
             Math.abs(arr1[1] - arr2[1])])
