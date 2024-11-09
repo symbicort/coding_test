0ab0ba8d930e4a13021654560500f819240759b2
symbicort <jeongwonlee0@gmail.com>
2024-11-09 23:37:34 +0900

    코딩 문제 추가

--- solve_problem/11005.js
+++ solve_problem/11005.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 let [num, divider] = fs.readFileSync("example.txt").toString().trim().split(" ");
 
-for(num1 of num){
-    console.log(num1)
+for(data of num){
+    console.log(data)
 }
 
